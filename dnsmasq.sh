faa9d9a4ad4d2485f8143231e765b949562933eb
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-08-16 17:27:51 +0800

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -25,11 +25,8 @@ listen-address=127.0.0.1
 cache-size=1000
 dns-forward-max=1000
 
-###本地地址###
-address=/localhost/127.0.0.1
-address=/localhost/::1
 
-###奈飞地址###
+###奈飞地址###########################
 address=/fast.com/$nfip
 address=/netflix.com/$nfip
 address=/netflix.net/$nfip
@@ -37,11 +34,11 @@ address=/nflxext.com/$nfip
 address=/nflximg.net/$nfip
 address=/nflxso.net/$nfip
 address=/nflxvideo.net/$nfip
-###香港TVB###
+###香港TVB#############################
 address=/mytvsuper.com/$nfip
 address=/tvb.com/$nfip
 address=/ads.tvb.com/127.0.0.1
-###台湾动画疯###
+###台湾动画疯##########################
 address=/gamer.com.tw/$twip
 address=/bahamut.com.tw/$twip
 address=/hinet.net/$twip
@@ -49,22 +46,22 @@ address=/fbcdn.net/$twip
 address=/gvt1.com/$twip
 address=/digicert.com/$twip
 address=/viblast.com/$twip
-###台湾B站###
+###台湾B站############################
 address=/bilibili.com/$twip
 address=/hdslb.com/$twip
-###台湾Line TV###
+###台湾Line TV########################
 address=/line.me/$twip
 address=/line-apps.com/$twip
-###日本AbemaTV###
+###日本AbemaTV########################
 address=/abema.tv/$jpip
 address=/ameblo.jp/$jpip
 address=/akamaized.net/$jpip
-####日本TVer####
+####日本TVer##########################
 address=/tver.jp/$jpip
 address=/amazonaws.com/$jpip
 address=/yahoo.co.jp/$jpip
 address=/brightcove.com/$jpip
-####日本niconico####
+####日本niconico######################
 address=/nicovideo.jp/$jpip
 address=/nimg.jp/$jpip
 ">/etc/dnsmasq.conf
