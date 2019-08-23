fc3e45677dfeb1798aeedafb8e91df549cdd5785
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-08-24 04:40:11 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -4,10 +4,10 @@ apt update
 apt install -y dnsmasq 
 systemctl enable dnsmasq
 #wget -N --no-check-certificate "https://raw.githubusercontent.com/gougogoal/ssr/manyuser/dnsmasq.sh"
-chmod 755 /root/plane/dnsmasq.sh
-sh /root/plane/dnsmasq.sh
+chmod 755 /root/ssr/dnsmasq.sh
+sh /root/ssr/dnsmasq.sh
 #添加定时脚本
-echo "* */1 * * * root /root/plane/dnsmasq.sh">>/etc/crontab
+echo "* */1 * * * root /root/ssr/dnsmasq.sh">>/etc/crontab
 #修改DNS，并设置为只读
 echo "nameserver 127.0.0.1">/etc/resolv.conf
 chattr +i /etc/resolv.conf
