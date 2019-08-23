7da66cecd291c750f72ce23e728c68788f64633b
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-08-23 23:40:47 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -42,4 +42,4 @@ MANAGE_PASS = "ss233333333"
 # if you want manage in other server you should set this value to global ip
 MANAGE_BIND_IP = "127.0.0.1"
 # make sure this port is idle
-MANAGE_PORT = 5678
+MANAGE_PORT = 5000
