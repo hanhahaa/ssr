219d9baa11a46aeedc76c1fd6dc5d59387b7b80f
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-08-26 11:58:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,6 +20,39 @@ systemctl restart ssr
 echo "sshd: ALL">/etc/hosts.allow
 #修改时区
 timedatectl set-timezone Asia/Shanghai
-
 #添加定时重启计划
 echo "0 3 * * * root reboot">>/etc/crontab
+##BBR以及内核优化
+echo "
+* soft nofile 51200
+* hard nofile 51200
+">>/etc/security/limits.conf
+echo "
+#开启BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv4.ip_forward=1
+#关闭IPV6
+net.ipv6.conf.all.disable_ipv6 = 1
+#系统及TCP优化
+vm.swappiness=0
+fs.file-max = 51200
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+">>/etc/sysctl.conf
+
