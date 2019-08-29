eb101c87cd906c4ca7a639b881797c697b951988
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-08-29 20:37:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
 
-apt update
-#安装环境
-apt install python-pip git libssl-dev python-dev libffi-dev software-properties-common vim -y
-add-apt-repository ppa:ondrej/php -y 
-apt install libsodium-dev -y
-#下载代码
-git clone -b manyuser https://github.com/GouGoGoal/ssr
-cd ssr
-pip install -r requirements.txt
-cp apiconfig.py userapiconfig.py
-cp config.json user-config.json
-#传入nodeid参数
-sed -i "2s/0/$1/" userapiconfig.py
-#添加服务
-cp -r /root/ssr/ssr.service /etc/systemd/system/
-systemctl enable ssr
-systemctl restart ssr
-echo "sshd: ALL">/etc/hosts.allow
-#修改时区
-timedatectl set-timezone Asia/Shanghai
-#添加定时重启计划
-echo "0 3 * * * root reboot">>/etc/crontab
+
+
 ##BBR以及内核优化
 echo "
 * soft nofile 51200
@@ -55,4 +35,28 @@ net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
 net.ipv4.tcp_mtu_probing = 1
 ">>/etc/sysctl.conf
+sysctl -p
+apt update
+#安装环境
+apt install python-pip git libssl-dev python-dev libffi-dev software-properties-common vim -y
+add-apt-repository ppa:ondrej/php -y 
+apt install libsodium-dev -y
+#下载代码
+git clone -b manyuser https://github.com/GouGoGoal/ssr
+cd ssr
+pip install -r requirements.txt
+cp apiconfig.py userapiconfig.py
+cp config.json user-config.json
+#传入nodeid参数
+sed -i "2s/0/$1/" userapiconfig.py
+#添加服务
+cp -r /root/ssr/ssr.service /etc/systemd/system/
+systemctl enable ssr
+systemctl restart ssr
+echo "sshd: ALL">/etc/hosts.allow
+#修改时区
+timedatectl set-timezone Asia/Shanghai
+#添加定时重启计划
+echo "0 3 * * * root reboot">>/etc/crontab
+
 
