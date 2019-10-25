5cec7289020e6ce1935eec044265e52401c79b3e
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-10-25 09:53:25 +0800

    Update freeram.sh

--- freeram.sh
+++ freeram.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-
-if ['free -m | awk '/Mem/ {print $3}'' -gt 400]; then
+#单位为M，如果以G为单位改成free -g，内存使用到limit后释放
+used_ram=`free -m | awk '/Mem/ {print $3}'`
+limit_ram=400
+if [ $used_ram -gt $limit_ram ]; then
+       #内存释放执行的命令,重启docker之类的
        echo "重启SSR服务，释放内存"
        systemctl restart ssr
+       #systemctl restart docker
+       #docker restart ssr
+else
+       echo "内存无需释放"
 fi
-       
