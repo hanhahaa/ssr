0fa963e7ec0be62635825dda46a5267b61666756
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-10-25 17:12:01 +0800

    Update freeram.sh

--- freeram.sh
+++ freeram.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-
 #Swap使用到limit_swap时释放内存，设置swappiness=0
 used_swap=`free -m | awk '/Swap/ {print $3}'`
-limit_swap=50
+limit_swap=1
 if [ $used_swap -gt $limit_swap ]; then
        #内存释放执行的命令,重启docker之类的
        echo "重启SSR服务，释放内存"
        systemctl restart ssr
+       swapoff -a && swapon -a
 else
        echo "内存无需释放"
 fi
