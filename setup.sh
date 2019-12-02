2ad32b80d20eec8de997b72867dbba9ff245308f
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-02 22:22:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,50 +42,33 @@ read -s -n1 -p "安装完毕，非游戏机请按任意键优化tcp连接"
 echo "
 #关闭IPV6
 net.ipv6.conf.all.disable_ipv6 = 1
+#开启BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
 #开启内核转发
 net.ipv4.ip_forward=1
 #优先使用ram
 vm.swappiness=0
-#开启BBR
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
-#对于一个新建连接，内核要发送多少个 SYN 连接请求才决定放弃。
-net.ipv4.tcp_syn_retries = 1
-#对于远端的连接请求SYN，内核会发送SYN ＋ ACK数据报，以确认收到上一个 SYN连接请求包
-net.ipv4.tcp_synack_retries = 1
-#TCP发送keepalive探测消息的间隔时间（秒），用于确认TCP连接是否有效。
-net.ipv4.tcp_keepalive_time = 600
-#TCP发送keepalive探测消息的间隔时间（秒），用于确认TCP连接是否有效。
-net.ipv4.tcp_keepalive_probes = 3
-#探测消息未获得响应时，重发该消息的间隔时间（秒）。
-net.ipv4.tcp_keepalive_intvl =15
-#在丢弃激活(已建立通讯状况)的TCP连接之前﹐需要进行多少次重试
-net.ipv4.tcp_retries2 = 5
-#对于本端断开的socket连接，TCP保持在FIN-WAIT-2状态的时间
-net.ipv4.tcp_fin_timeout = 2
-#系统在同时所处理的最大 timewait sockets 数目
-net.ipv4.tcp_max_tw_buckets = 36000
-#打开快速 TIME-WAIT sockets 回收
-net.ipv4.tcp_tw_recycle = 1
-#对于那些依然还未获得客户端确认的连接请求﹐需要保存在队列中最大数目。
-net.ipv4.tcp_max_syn_backlog = 16384
-#发送缓存设置
-net.ipv4.tcp_wmem = 8192 131072 16777216
-#接收缓存设置
-net.ipv4.tcp_rmem = 32768 131072 16777216
-#
-net.ipv4.tcp_mem = 786432 1048576 1572864
-
+#TCP优化
 fs.file-max = 51200
 net.core.rmem_max = 67108864
 net.core.wmem_max = 67108864
 net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
 net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
 net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
 net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
 net.ipv4.tcp_mtu_probing = 1
-">/etc/sysctl.conf
+">>/etc/sysctl.conf
 sysctl -p
 ###########
 
