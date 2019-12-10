a40f583209c7271ee829dd6b6f20d771d800ca72
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-10 12:51:44 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -8,6 +8,9 @@ chmod 755 /root/ssr/dnsmasq.sh
 bash /root/ssr/dnsmasq.sh
 #添加定时脚本
 echo "* * * * * root /root/ssr/dnsmasq.sh">>/etc/crontab
+#更改DNS，并设置为只读
+echo "nameserver 127.0.0.1">/etc/resolv.conf
+chattr +i /etc/resolv.conf
 
 systemctl restart ssr
 
