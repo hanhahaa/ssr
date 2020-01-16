746a2303df0b58e7441f10a3b0af80fa61456cde
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-01-16 16:29:00 +0800

    Update freeram.sh

--- freeram.sh
+++ freeram.sh
@@ -3,8 +3,8 @@ used_swap=`free -m | awk '/Swap/ {print $3}'`
 limit_swap=10
 if [ $used_swap -gt $limit_swap ]; then
        #内存释放执行的命令,重启docker之类的
-       echo "重启SSR服务，释放内存"
-       systemctl restart ssr
+       echo "重启SSR、V2ray服务，释放内存"
+       systemctl restart ssr v2ray
        swapoff -a && swapon -a
 else
        echo "内存无需释放"
