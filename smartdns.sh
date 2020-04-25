5dadf5569da7522f1896a8a828c02698403646ba
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-04-25 18:32:08 +0800

    Update smartdns.sh

--- smartdns.sh
+++ smartdns.sh
@@ -9,20 +9,22 @@ usip=`ping -c1 unlock.us.soulout.club|awk -F'[(|)]' 'NR==1{print $2}'`
 #奈飞IP，就近解锁，美国鸡就写usip
 nfip=$hkip
 
-#检查swap使用量，如果超过物理内存的20%，则重启服务
+#释放内存
 freeram() {
 #获取总共内存
 totally_ram=`free -m | awk '/Mem/ {print $2}'`
 #获取当前使用的swap量
 used_swap=`free -m | awk '/Swap/ {print $3}'`
-#如果总内存/swap用量小于5，即swap超过物理内存的20%
-if [ $totally_ram/$used_swap -lt 5 ]; then
+#如果swap=0,则退出脚本，如果总内存/swap用量小于10，即swap超过物理内存的10%
+if [ "$used_swap" -eq 0 ]; then
+        exit 
+elif [ "$totally_ram"/"$used_swap" -lt 10 ]; then
        #内存释放执行的命令,重启docker之类的
        echo "重启SSR、V2ray服务，释放内存"
        systemctl restart ssr v2ray
        swapoff -a && swapon -a
-else
-       echo "内存无需释放"
+else 
+       exit
 fi
 }
 
