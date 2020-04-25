e5d02d388b14f98e6c09313af6c444e820a1f8c5
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-04-25 12:20:42 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #获取脚本相对路径
 DIR=`dirname $0`
-chmod +x  smartdns
+chmod +x  $DIR/smartdns
 mv $DIR/smartdns /usr/bin
 #加载服务
 mv $DIR/smartdns.service /etc/systemd/system
@@ -17,9 +17,18 @@ bash /etc/smartdns.sh
 echo "* * * * * root /etc/smartdns.sh">>/etc/crontab
 #劫持本机所有的UDP *:53 的流量到127.0.0.1:53，并将规则开机自启
 iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-sed -i '$i\iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53' /etc/rc.local
+
+#判断是否有/etc/rc.local，若没有则添加
+if [ ! -f "/etc/rc.local" ]; then
+    echo '#!/bin/sh -e
+iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
+exit 0' >/etc/rc.local
+    chmod +x /etc/rc.local
+    systemctl restart rc.local
+else
+    sed -i '$i\iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53' /etc/rc.local
+fi
 
 #更改DNS，并设置为只读
 echo "nameserver 127.0.0.1">/etc/resolv.conf
 chattr +i /etc/resolv.conf
-rm -rf $DIR
