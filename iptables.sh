0b9b8d0e6bba7f2dc3991e31403638c258f0add1
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-05-07 12:41:30 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -5,7 +5,7 @@
 #需要修改内核参数支持转发；在 /etc/sysctl.conf 添加一行  net.ipv4.ip_forward=1 ，之后 sysctl -p
 #并设置定时任务，每分钟执行一次，不会请谷歌
 #与iptables转发链规则冲突，可添加规则到reset_iptables函数，如无必要，docker请使用host模式
-if [ `whoami` = "root" ];then
+if [ `whoami` -ne "root" ];then
 	echo "请以root用户执行"
 	exit 0
 fi
