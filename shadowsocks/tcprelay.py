8e95978efa14278911871952a6f225c05859f265
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-05-16 13:22:00 +0800

    Update tcprelay.py

--- shadowsocks/tcprelay.py
+++ shadowsocks/tcprelay.py
@@ -954,9 +954,9 @@ class TCPRelayHandler(object):
         else:
             bind_addr = self._accept_address[0]
 
-        bind_addr = bind_addr.replace("::ffff:", "")
-        if bind_addr in self._ignore_bind_list:
-            bind_addr = None
+         bind_addr = bind_addr.replace("::ffff:", "")
+         #if bind_addr in self._ignore_bind_list:
+         #   bind_addr = None
 
         if self._is_relay:
             bind_addr = None
