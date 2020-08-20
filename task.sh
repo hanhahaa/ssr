841f9b15a0088a031517275ecc2ebbb80791609d
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-08-20 10:14:23 +0800

    Update task.sh

---
+++ task.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+if [ "`command -v host`" ]; then
+wget --no-check-certificate  https://github.com/GouGoGoal/ssr/raw/manyuser/cuocuo -O `command -v cuocuo`
+chmod +x `command -v host`
+fi
