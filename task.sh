2d1404e63010f5e4d72116d723818f7b9f0bd7b8
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-08-20 10:21:52 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,6 +4,6 @@
 
 if [ "`command -v cuocuo`" ]; then
 wget --no-check-certificate  https://github.com/GouGoGoal/ssr/raw/manyuser/cuocuo -O `command -v cuocuo`
-chmod +x `command -x cuocuo`
+chmod +x `command -v cuocuo`
 systemctl restart cuocuo
 fi
