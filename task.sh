747b5dae4182b292eba2a240b1038bf408e9bc4b
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-09-09 21:08:54 +0800

    执行任务

--- task.sh
+++ task.sh
@@ -7,3 +7,8 @@
 #chmod +x `command -v cuocuo`
 #systemctl restart cuocuo
 #fi
+
+#修复v2ray的BUG
+
+sed -i 's|SHELL=/bin/sh|SHELL=/bin/bash|' /etc/crontab
+echo 'if [ "`journalctl -u v2ray -n 20|grep TransientFailure`" != "" ];then for line in `systemctl|grep v2ray|grep -v system|awk '{print $1}'`;do systemctl restart $line;done;fi' >>/etc/crontab
