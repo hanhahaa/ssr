00173a9cd6e00cad6ae57f08c668cb468ae4b692
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-09-10 09:11:42 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -10,5 +10,6 @@
 
 #修复v2ray的BUG
 
-sed -i 's|SHELL=/bin/sh|SHELL=/bin/bash|' /etc/crontab
-echo 'if [ "`journalctl -u v2ray -n 20|grep TransientFailure`" != "" ];then for line in `systemctl|grep v2ray|grep -v system|awk '{print $1}'`;do systemctl restart $line;done;fi' >>/etc/crontab
+#sed -i 's|SHELL=/bin/sh|SHELL=/bin/bash|' /etc/crontab
+sed -i '/.*TransientFailure.*/d' /etc/crontab
+echo "* * * * * root if [ \"\`journalctl -u v2ray -n 20|grep TransientFailure\`\" != \"\" ];then for line in \`systemctl|grep v2ray|grep -v system|awk '{print \$1}'\`;do systemctl restart \$line;done;fi" >>/etc/crontab
