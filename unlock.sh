fd22c23a30dec474f138334f335cbb8e5f033524
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-09-17 13:25:00 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -3,23 +3,22 @@
 #获取脚本相对路径
 DIR=`dirname $0`
 chmod +x  $DIR/smartdns
-mv $DIR/smartdns /usr/bin
+mv $DIR/smartdns /usr/sbin
 #加载服务
 echo "[Unit]
-Description=smartdns server
-After=network.target
-Before=ssr.service v2ray.service docker.service
+Description=SmartDNS server
+After=network.target NetworkManager.service
+Before=rc-local.service
 
 [Service]
 Type=simple
-PIDFile=/run/smartdns.pid
-ExecStart=/usr/bin/smartdns -f -c /etc/smartdns.conf
-ExecStartPre=/bin/sleep 0.1
+ExecStart=/usr/sbin/smartdns -f -c /etc/smartdns.conf
 Restart=always
 
 [Install]
 WantedBy=multi-user.target" >/etc/systemd/system/smartdns.service
 #服务开机自启
+systemctl daemon-reload
 systemctl enable smartdns
 #给脚本执行权限
 chmod +x $DIR/smartdns.sh
@@ -30,5 +29,3 @@ mv $DIR/ban.conf /etc/ban.conf
 bash /etc/smartdns.sh
 #添加定时脚本
 echo "* * * * * root /etc/smartdns.sh">>/etc/crontab
-#脚本自删除
-rm -f  $DIR/unlock.sh
